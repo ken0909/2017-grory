896a9bff8832aedeb7eadeca23dbfb913f00be7a
kengo0909 <shiva0909ken@gmail.com>
2018-01-19 20:36:30 +0900

    created authentication middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,66 @@
+import * as auth from "../modules/auth";
+import { firebaseAuth } from "../utils/FirebaseUtil";
+import { push } from "react-router-redux";
+
+export default store => next => action => {
+  const { email, password, name, user } = action.payload
+  switch (action.type) {
+    case auth.SIGN_IN:
+      next(action)
+      firebaseAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        store.dispatch(auth.signInSuccess({ user }))
+        store.dispatch(auth.updateProfile({ user, name }))
+      })
+      .catch(error => store.dispatch(auth.signInFailure(error)));
+      break;
+    case auth.SIGN_IN_SUCCESS:
+      next(action)
+      store.dispatch(push('/'))
+      break
+    case auth.LOG_IN:
+      next(action)
+      firebaseAuth.signInWithEmailAndPassword(email, password)
+      .then(user => store.dispatch(auth.logInSuccess({ user })))
+      .catch(error => store.dispatch(auth.logInFailure(error)));
+      break;
+    case auth.LOG_IN_SUCCESS:
+      next(action)
+      store.dispatch(push('/'))
+      break;
+    case auth.UPDATE_PROFILE:
+      next(action)
+      user.updateProfile({
+        displayName: name
+      })
+      .then(() => store.dispatch(auth.updateProfileSuccess({ name: user.displayName })))
+      .catch(error => store.dispatch(auth.updateProfileFailure(error)))
+      break
+    case auth.LOG_OUT:
+      next(action)
+      firebaseAuth.signOut().then(() => store.dispatch(auth.logOutSuccess({})))
+      break;
+    case auth.LOG_OUT_SUCCESS:
+      next(action)
+      store.dispatch(push('/login'))
+      break;
+    case auth.LOAD_LOG_IN_STATE:
+      next(action)
+      firebaseAuth.onAuthStateChanged(user => {
+        if (user) {
+          store.dispatch(auth.logInSuccess({ user }))
+          // this.userRef = firebaseDbRef(`user/${user.uid}`);
+          // this.userRef.on('value', snapshot =>
+          //   this.props.actions.setUserDistance({
+          //     distance: snapshot.val().distance
+          //   })
+          // );
+        }
+      });
+      break;
+    default:
+      next(action)
+      break;
+  }
+}
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,12 +1,18 @@
 import { createAction, handleActions } from 'redux-actions';
 
-const TOGGLE_AUTH = 'TOGGLE_AUTH';
-const SIGN_IN = 'SIGN_IN';
-const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
-const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
-const LOG_IN = 'LOG_IN';
-const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
-const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+export const TOGGLE_AUTH = 'TOGGLE_AUTH';
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
+export const LOG_IN = 'LOG_IN';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE'
+export const UPDATE_PROFILE_SUCCESS = 'UPDATE_PROFILE_SUCCESS'
+export const UPDATE_PROFILE_FAILURE = 'UPDATE_PROFILE_FAILURE'
+export const LOG_OUT = 'LOG_OUT'
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS'
+export const LOAD_LOG_IN_STATE = 'LOAD_LOG_IN_STATE'
 
 export const toggleAuth = createAction(TOGGLE_AUTH);
 export const signIn = createAction(SIGN_IN);
@@ -15,6 +21,12 @@ export const signInFailure = createAction(SIGN_IN_FAILURE);
 export const logIn = createAction(LOG_IN);
 export const logInSuccess = createAction(LOG_IN_SUCCESS);
 export const logInFailure = createAction(LOG_IN_FAILURE);
+export const updateProfile = createAction(UPDATE_PROFILE)
+export const updateProfileSuccess = createAction(UPDATE_PROFILE_SUCCESS)
+export const updateProfileFailure = createAction(UPDATE_PROFILE_FAILURE)
+export const logOut = createAction(LOG_OUT)
+export const logOutSuccess = createAction(LOG_OUT_SUCCESS)
+export const loadLogInState = createAction(LOAD_LOG_IN_STATE)
 
 const initialState = {
   logInMode: true,
@@ -22,7 +34,8 @@ const initialState = {
   isLoading: false,
   name: 'ゲスト',
   message: '',
-  error: false
+  error: false,
+  uid: ''
 };
 
 export default handleActions(
@@ -39,7 +52,8 @@ export default handleActions(
       ...state,
       isLogIn: true,
       isLoading: false,
-      name: action.payload.name
+      name: action.payload.user.displayName,
+      uid: action.payload.user.uid
     }),
     [SIGN_IN_FAILURE]: (state, action) => ({
       ...state,
@@ -56,7 +70,8 @@ export default handleActions(
       ...state,
       isLogIn: true,
       isLoading: false,
-      name: action.payload.name
+      name: action.payload.user.displayName,
+      uid: action.payload.user.uid
     }),
     [LOG_IN_FAILURE]: (state, action) => ({
       ...state,
@@ -64,6 +79,33 @@ export default handleActions(
       isLoading: false,
       message: action.payload.message,
       error: action.error
+    }),
+    [UPDATE_PROFILE]: state => ({
+      ...state,
+      isLoading: true
+    }),
+    [UPDATE_PROFILE_SUCCESS]: (state, action) => ({
+      ...state,
+      isLoading: false,
+      name: action.payload.name
+    }),
+    [UPDATE_PROFILE_FAILURE]: (state, action) => ({
+      ...state,
+      isLoading: false,
+      message: action.payload.message,
+      error: action.error
+    }),
+    [LOG_OUT]: state => ({
+      ...state,
+      isLoading: true
+    }),
+    [LOG_OUT_SUCCESS]: state => ({
+      ...state,
+      isLoading: false,
+      isLogIn: false
+    }),
+    [LOAD_LOG_IN_STATE]: state => ({
+      ...state
     })
   },
   initialState
